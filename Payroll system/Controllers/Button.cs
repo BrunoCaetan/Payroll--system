f3d8146950ea859fac718663792739f6f5a48eeb
BrunoCaetan <brunocaetanorsilva@gmail.com>
2023-11-23 16:49:49 -0300

    Criação de Buttons

---
+++ Payroll system/Controllers/Button.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Payroll_system.Models;
+using System.Diagnostics;
+
+namespace Payroll_system.Controllers
+{
+    public class ButtonController : Controller
+    {
+        public IActionResult EmpresaEditar()
+        { 
+            return View(); 
+        }
+
+        public IActionResult FeriasEditar()
+        {
+            return View();
+        }
+
+        public IActionResult FuncionarioCadastrar()
+        {
+            return View();
+        }
+
+        public IActionResult FuncionarioEditar()
+        {
+            return View();
+        }
+
+        public IActionResult FuncionarioExcluir()
+        {
+            return View();
+        }
+
+        public IActionResult LicencaEditar()
+        {
+            return View();
+        }
+
+        public IActionResult HoleriteGerado()
+        {
+            return View();
+        }
+    }
+}
--- Payroll system/Controllers/HomeController.cs
+++ Payroll system/Controllers/HomeController.cs
@@ -39,6 +39,12 @@ namespace Payroll_system.Controllers
             return View();
         }
 
+        public IActionResult EmpresaEditar()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
