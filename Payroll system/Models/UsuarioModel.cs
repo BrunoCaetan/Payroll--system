5903895e98d083a13e2bb2eaaaf90e002fbab100
BrunoCaetan <brunocaetanorsilva@gmail.com>
2023-11-23 23:57:04 -0300

    Oi celio

--- Payroll system/Models/UsuarioModel.cs
+++ Payroll system/Models/UsuarioModel.cs
@@ -1,5 +1,4 @@
-﻿using Payroll_system.Enums;
-
+﻿
 namespace Payroll_system.Models
 {
     public class UsuarioModel
@@ -8,7 +7,6 @@ namespace Payroll_system.Models
         public string Name { get; set; }
         public string Login {  get; set; }
         public string Email { get; set; }
-        public PerfilEnum Perfil { get; set; }
         public string Senha { get; set; }
         public DateTime DataCadastro { get; set; }
         public DateTime? DataAtualizacao { get; set; }
